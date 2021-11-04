d27959fae3adb21f42898491f5605c41cd0da5ec
Dan Allen <dan.j.allen@gmail.com>
2021-11-03 21:11:52 -0600

    upgrade Antora to 3.0.0-beta.1

--- docsearch/publish-config-extension.js
+++ docsearch/publish-config-extension.js
@@ -4,18 +4,18 @@ const { promises: fsp } = require('fs')
 const ospath = require('path')
 
 /**
- * An Antora pipeline extension that generates the docsearch config file from a Handlebars template and
- * publishes it with the site, where the scraper job can retrieve it.
+ * An Antora extension that generates the docsearch config file from a Handlebars template and publishes it with the
+ * site, where the scraper job can retrieve it.
  */
-module.exports.register = (pipeline) => {
+module.exports.register = () => {
   const handlebars = require(
     require.resolve('handlebars', {
-      paths: [ospath.dirname(require.resolve('@antora/page-composer', { paths: pipeline.module.paths }))]
+      paths: [ospath.dirname(require.resolve('@antora/page-composer', { paths: this.module.paths }))]
     })
   ).create()
   handlebars.registerHelper('eq', (a, b) => a === b)
 
-  pipeline.on('beforePublish', async ({ playbook, contentCatalog, siteCatalog }) => {
+  this.on('beforePublish', async ({ playbook, contentCatalog, siteCatalog }) => {
     const configFile = ospath.join(__dirname, 'config.json')
     const templateSrc = await fsp.readFile(configFile + '.hbs', 'utf8')
     const template = handlebars.compile(templateSrc, { noEscape: true, preventIndent: true, srcName: 'config.json.hbs' })
