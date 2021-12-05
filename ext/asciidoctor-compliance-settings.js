765df5b485c278085ff031310b04e21c6e6f9d4a
Dan Allen <dan.j.allen@gmail.com>
2021-12-05 16:29:01 -0700

    upgrade to Antora 3.0.0-beta.6

--- ext/asciidoctor-compliance-settings.js
+++ ext/asciidoctor-compliance-settings.js
@@ -1,3 +1,7 @@
 'use strict'
 
-module.exports.register = () => (global.Opal.Asciidoctor.Compliance.underline_style_section_titles = false)
+module.exports.register = function () {
+  this.once('contextStarted', () => {
+    global.Opal.Asciidoctor.Compliance.underline_style_section_titles = false
+  })
+}
