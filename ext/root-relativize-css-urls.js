877bd8b6aeb2110e43f9bfadf8f9cd205eea7a36
Dan Allen <dan.j.allen@gmail.com>
2021-12-22 23:58:12 -0700

    remove obsolete extension to convert relative URLs in CSS to root relative

--- ext/root-relativize-css-urls.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports.register = function () {
-  this.once('uiLoaded', ({ uiCatalog }) => {
-    const siteCss = uiCatalog.findByType('asset').find((it) => it.path === 'css/site.css')
-    if (siteCss) siteCss.contents = Buffer.from(siteCss.contents.toString().replace(/url\(\.\.[/]/g, 'url(/_/'))
-  })
-}
