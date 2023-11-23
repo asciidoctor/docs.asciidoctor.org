0734e4fd3c8c497ce52d6f83e60e6ee48f13a9f7
Dan Allen <dan.j.allen@gmail.com>
2023-11-23 01:45:19 -0700

    fix index URL for about component; include all versionless components except ROOT

--- docsearch/publish-config-extension.js
+++ docsearch/publish-config-extension.js
@@ -10,15 +10,14 @@ const ospath = require('node:path')
 module.exports.register = function () {
   const handlebars = this.require('handlebars').create()
   handlebars.registerHelper('eq', (a, b) => a === b)
+  handlebars.registerHelper('not', (val) => !val)
   handlebars.registerHelper('or', (a, b) => a || b)
 
   this.on('beforePublish', async ({ playbook, contentCatalog, siteCatalog }) => {
     const configFile = ospath.join(__dirname, 'config.json')
     const templateSrc = await fsp.readFile(configFile + '.hbs', 'utf8')
     const template = handlebars.compile(templateSrc, { noEscape: true, preventIndent: true, srcName: 'config.json.hbs' })
-    const components = contentCatalog
-      .getComponentsSortedBy('name')
-      .filter((component) => component.name === 'about' || component.latest.version)
+    const components = contentCatalog.getComponentsSortedBy('name').filter((component) => component.name !== 'ROOT')
     const stopPages = contentCatalog.getPages((page) => {
       return page.out && ('page-archived' in page.asciidoc.attributes || 'page-noindex' in page.asciidoc.attributes)
     })
