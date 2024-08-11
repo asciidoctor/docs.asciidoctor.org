f240887c59dceab271f9a49d30f38c062dffae56
Dan Allen <dan.j.allen@gmail.com>
2024-08-11 15:56:42 -0600

    consolidate code in promote-favicon-to-root extension

--- ext/promote-favicon-to-root.js
+++ ext/promote-favicon-to-root.js
@@ -4,13 +4,12 @@ module.exports.register = function () {
   this.once('uiLoaded', ({ siteCatalog, uiCatalog }) => {
     const favicon = uiCatalog.findByType('asset').find((it) => it.path === 'img/favicon.ico')
     if (!favicon) return
-    const rootFavicon = new favicon.constructor({
+    uiCatalog.addFile(new favicon.constructor({
       contents: favicon.contents,
       out: { dirname: '', basename: 'favicon.ico', path: 'favicon.ico' },
       path: 'favicon.ico',
       stat: favicon.stat,
       type: 'static',
-    })
-    uiCatalog.addFile(rootFavicon)
+    }))
   })
 }
